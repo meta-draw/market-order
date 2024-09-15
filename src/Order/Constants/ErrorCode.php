9b7fa410c14f8c0cdd484f71c406734c46e7a5bd
XuchaoChen <yaphets.xuchao@gmail.com>
2024-09-15 21:53:39 +0900

    General error code contains send trade error

--- src/Order/Constants/ErrorCode.php
+++ src/Order/Constants/ErrorCode.php
@@ -10,4 +10,28 @@ enum ErrorCode: int
     case ListingItemExisted = 42211;
 
     case OrderNotFound = 42404;
+
+    case SEND_TRADE_ERROR_UNKNOWN = 44002;
+
+    case SEND_TRADE_ERROR_TIMEOUT = 44016;
+
+    case SEND_TRADE_ERROR_INVALID_TRADE_URL = 44015;
+
+    case SEND_TRADE_ERROR_ITEM_NOT_EXISTED = 44026;
+
+    case SEND_TRADE_ERROR_LIMIT_EXCEEDED = 44025;
+
+    case SEND_TRADE_ERROR_SERVICE_UNAVAILABLE = 44020;
+
+    public static function fromSendTradeErrorCode(SendTradeErrorCode $sendTradeErrorCode): self
+    {
+        return match ($sendTradeErrorCode) {
+            SendTradeErrorCode::UNKNOWN => self::SEND_TRADE_ERROR_UNKNOWN,
+            SendTradeErrorCode::TIMEOUT => self::SEND_TRADE_ERROR_TIMEOUT,
+            SendTradeErrorCode::INVALID_TRADE_URL => self::SEND_TRADE_ERROR_INVALID_TRADE_URL,
+            SendTradeErrorCode::ITEM_NOT_EXISTED => self::SEND_TRADE_ERROR_ITEM_NOT_EXISTED,
+            SendTradeErrorCode::LIMIT_EXCEEDED => self::SEND_TRADE_ERROR_LIMIT_EXCEEDED,
+            SendTradeErrorCode::SERVICE_UNAVAILABLE => self::SEND_TRADE_ERROR_SERVICE_UNAVAILABLE,
+        };
+    }
 }
