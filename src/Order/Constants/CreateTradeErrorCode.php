51c87f7b66b1a08535708ea628ecb5bd986c2c60
XuchaoChen <yaphets.xuchao@gmail.com>
2024-09-15 21:33:43 +0900

    Add send trade error code

---
+++ src/Order/Constants/CreateTradeErrorCode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SkinsPay\Order\Constants;
+
+enum CreateTradeErrorCode: int
+{
+    case UNKNOWN = 2;
+    case TIMEOUT = 16;
+    case INVALID_TRADE_URL = 15;
+    case ITEM_NOT_EXISTED = 26;
+    case LIMIT_EXCEEDED = 25;
+    case SERVICE_UNAVAILABLE = 20;
+}
