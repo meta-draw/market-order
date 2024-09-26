7e3c1c90fb4f57b115509e09183ef929148998c8
Jeffrey-Lin-23 <jeffreylin.job@gmail.com>
2024-09-26 14:46:19 +1000

    add invalid steamId & cookies

--- src/Order/Constants/ErrorCode.php
+++ src/Order/Constants/ErrorCode.php
@@ -7,6 +7,8 @@ enum ErrorCode: int
     case PriceExceedLimit = 42201;
     case InvalidListing = 42202;
     case InvalidInventory = 42203;
+    case InvalidSteamId = 42204;
+    case InvalidCookies = 42205;
 
     case ListingItemExisted = 42211;
 
