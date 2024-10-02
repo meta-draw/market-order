dcbb4128e502cd87cb9f4e50eafa41db31608cea
XuchaoChen <yaphets.xuchao@gmail.com>
2024-10-03 02:31:19 +0900

    Merge send trade errorcode

--- src/Order/Constants/ErrorCode.php
+++ src/Order/Constants/ErrorCode.php
@@ -13,4 +13,17 @@ enum ErrorCode: int
     case ListingItemExisted = 42211;
 
     case OrderNotFound = 42404;
+
+
+    /// Send Trade Related Error Code, Format should be 4XXXXX
+    case Unauthorized = 440100;
+    case Unknown = 450000;
+    case NetworkError = 450100;
+    case Failed = 440002;
+    case InvalidTradeUrl = 440015;
+    case Timeout = 440016;
+    case ItemNotExisted = 440026;
+    case LimitExceeded = 440025;
+    case ServiceUnavailable = 440020;
+
 }
